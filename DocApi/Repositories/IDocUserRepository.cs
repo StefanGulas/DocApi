078111d6923b7cb563425a3293cdfffb507c0410
StefanGulas <stefan.gulas@gmail.com>
2021-03-19 06:43:10 +0100

    Refactoring

--- DocApi/Repositories/IDocUserRepository.cs
+++ DocApi/Repositories/IDocUserRepository.cs
@@ -29,18 +29,5 @@ namespace DocApi.Repositories
         void ChangeRoleInDb(Role role, Role existingRole);
         void DeleteRoleInDb(Role role);
 
-        //IEnumerable<Document> GetDocumentsByUser(int userId);
-        //Document GetDocument(int userId, int documentId);
-        //void AddDocument(Guid userId, Document document);
-        //void UpdateDocument(Document document);
-        //void DeleteDocument(Document document);
-        //IEnumerable<User> GetUsers();
-        //User GetUser(int userId);
-        //IEnumerable<User> GetUsers(IEnumerable<int> userIds);
-        //void AddUser(User user);
-        //void DeleteUser(User user);
-        //void UpdateUser(User user);
-        //bool UserExists(int userId);
-        //bool Save();
     }
 }
